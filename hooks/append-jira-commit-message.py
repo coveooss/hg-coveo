4f23f1fb5ee08921292542a28e1479cc36dd6dbb
Jean-Francis Roy <jfroy@coveo.com>
2016-12-14 10:32:56 -0500

    Avoid using built-in 'str' as a variable name and make Python linters happy

--- hooks/append-jira-commit-message.py
+++ hooks/append-jira-commit-message.py
@@ -1,27 +1,32 @@
-import re,os,sys,mercurial,hglib
+import re
+import sys
+import textwrap
+import hglib
+
 
 class MissingConfig(Exception):
     pass
- 
+
+
 def append_jira_commit_message(repo, **kwargs):
     commitctx = repo.commitctx
     client = hglib.open('.')
-    
-    def printHelp():
-        print("""
-Append a Jira link to commit messages.
-        
-If the current branch (or bookmark) matches the Jira project, 
-and if the commit message doesn't already contain it,
-a link to the Jira is appended to commit messages.
-        
-.hgrc configuration:
-    [jira]
-    jira.project = Jira project name, e.g. JIRA
-    jira.url = Jira base url, e.g. https://jira.atlassian.com/browse/
-""")
-    
-    def getConfig(key):
+
+    def print_help():
+        print(textwrap.dedent("""
+            Append a Jira link to commit messages.
+
+            If the current branch (or bookmark) matches the Jira project,
+            and if the commit message doesn't already contain it,
+            a link to the Jira is appended to commit messages.
+
+            .hgrc configuration:
+                [jira]
+                jira.project = Jira project name, e.g. JIRA
+                jira.url = Jira base url, e.g. https://jira.atlassian.com/browse/
+            """))
+
+    def get_config(key):
         try:
             cfg = next(cfg for cfg in client.config("jira") if cfg[1] == key)
         except (StopIteration, hglib.error.CommandError):
@@ -31,45 +36,44 @@ a link to the Jira is appended to commit messages.
         return cfg[2]
 
     try:
-        jiraUrlBase = getConfig("jira.url")
+        jira_url_base = get_config("jira.url")
     except MissingConfig as err:
         print("ERROR: {}".format(err))
-        printHelp()
+        print_help()
         sys.exit(1)
 
-    def format_message(str, jira, whence):
-        jiraLink = "{0}/{1}".format(jiraUrlBase, jira)
-        if jiraLink in str:
-            return str
+    def format_message(message, jira, whence):
+        jira_link = "{0}/{1}".format(jira_url_base, jira)
+        if jira_link in message:
+            return message
         else:
-            print "\nAppended JIRA '{0}' to commit message (detected from {1}).\n".format(jiraLink, whence)
-            return "{0}\n\n{1}\n".format(str.strip(), jiraLink)
+            print("\nAppended JIRA '{0}' to commit message (detected from {1}).\n".format(jira_link, whence))
+            return "{0}\n\n{1}\n".format(message.strip(), jira_link)
 
     def rewrite_ctx(ctx, error):
         branch_name = ctx.branch()
-        commitMessage = ctx._text
+        commit_message = ctx._text
 
         found = extract_project_and_id(branch_name)
         if found:
-            commitMessage = format_message(commitMessage, found, "branch name")
+            commit_message = format_message(commit_message, found, "branch name")
         else:
             bookmarks, active = client.bookmarks()
             if active != -1:
                 bookmark_name = bookmarks[active][0]
                 found = extract_project_and_id(bookmark_name)
                 if found:
-                    commitMessage = format_message(commitMessage, found, "bookmark name")
-        
-        ctx._text = commitMessage            
+                    commit_message = format_message(commit_message, found, "bookmark name")
+
+        ctx._text = commit_message
         return commitctx(ctx, error)
 
     def extract_project_and_id(message):
-        jiraRegex = re.compile("""^[A-Z]+-\d+""")
-        match = jiraRegex.match(message)
+        jira_regex = re.compile(r"^[A-Z]+-\d+")
+        match = jira_regex.match(message)
         if match:
             return match.group(0)
         else:
             return None
- 
+
     repo.commitctx = rewrite_ctx
- 
